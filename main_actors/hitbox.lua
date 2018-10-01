00ca369c7a5f80f80c1b1c34a77a28c484600ea5
Sebastian Jensen <s.nesgaard@gmail.com>
2018-10-01 21:20:02 +0200

    Update hitboxes. Added lab tilemap and tilesets. Added golem enemy with animation

--- main_actors/hitbox.lua
+++ main_actors/hitbox.lua
@@ -1,5 +1,7 @@
 return {
     fencer_dash = {attack = {from = 5, to = 5}},
     fencer_cast = {cast = {from = 9, to = 9}},
-    gunner_cast = {cast = {from = 6, to = 6}}
+    gunner_cast = {cast = {from = 6, to = 6}},
+    golem_cast = {cast = {from = 7, to = 7}},
+    golem_dash = {attack = {from = 3, to = 3}}
 }

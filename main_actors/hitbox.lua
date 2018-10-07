cbdccc20e6d7860cef918f054e18ad71bb2d6495
Sebastian Jensen <s.nesgaard@gmail.com>
2018-10-07 19:17:14 +0200

    vampire prises

--- main_actors/hitbox.lua
+++ main_actors/hitbox.lua
@@ -3,5 +3,7 @@ return {
     fencer_cast = {cast = {from = 9, to = 9}},
     gunner_cast = {cast = {from = 6, to = 6}},
     golem_cast = {cast = {from = 7, to = 7}},
-    golem_dash = {attack = {from = 3, to = 3}}
+    golem_dash = {attack = {from = 3, to = 3}},
+    vampire_dash = {attack = {from = 4, to = 4}},
+    vampire_cast = {cast = {from = 8, to = 8}}
 }
